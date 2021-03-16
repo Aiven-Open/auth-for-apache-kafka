a0df06122ac823aa6a6744684eef9f1a89ce6b49
Andrey Pleskach <ples@aiven.io>
2021-03-16 13:45:00 +0100

    add serialVersionUID into JsonReaderException

--- src/main/java/io/aiven/kafka/auth/json/reader/JsonReaderException.java
+++ src/main/java/io/aiven/kafka/auth/json/reader/JsonReaderException.java
@@ -5,7 +5,11 @@
 package io.aiven.kafka.auth.json.reader;
 
 public class JsonReaderException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     public JsonReaderException(final String message, final Throwable cause) {
         super(message, cause);
     }
+
 }
